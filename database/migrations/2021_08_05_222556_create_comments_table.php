30dd99739debcfe35b5c9c31043ff413123eb780
mion <parisdaisy0109@gmail.com>
2021-08-07 02:41:38 +0900

    CommentsMigrationファイルのpost_idをbigintegerに変更

--- database/migrations/2021_08_05_222556_create_comments_table.php
+++ database/migrations/2021_08_05_222556_create_comments_table.php
@@ -15,7 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('post_id')->unsigned()->nullable(false);
+            $table->biginteger('post_id')->unsigned()->nullable(false);
             $table->biginteger('user_id')->unsigned()->nullable(false);
             $table->string('content')->nullable(false);
             $table->timestamps();
