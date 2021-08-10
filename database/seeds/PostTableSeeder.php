c2cc1884a9557bb6289091f2c13dc78625046475
hikaru.tsujinaka <hikaru.tsujinaka@gmail.com>
2021-08-10 11:41:56 +0000

    postsテーブル、シーダーの修正

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -11,36 +11,35 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('posts')->insert([
-            'user_id' => 1,
-            'title' => 'タイトル1',
-            'message' => '本文1',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
-        ]);
-            
-        DB::table('posts')->insert([
-            'user_id' => 2,
-            'title' => 'タイトル2',
-            'message' => '本文2',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
-        ]);
-            
-        DB::table('posts')->insert([
-            'user_id' => 3,
-            'title' => 'タイトル3',
-            'message' => '本文3',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
-        ]);
-        
-        DB::table('posts')->insert([
-            'user_id' => 4,
-            'title' => 'タイトル4',
-            'message' => '本文4',
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
+       DB::table('posts')->insert([
+            [
+                'user_id' => 1,
+                'title' => 'タイトル1',
+                'message' => '本文1',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' => 2,
+                'title' => 'タイトル2',
+                'message' => '本文2',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' => 3,
+                'title' => 'タイトル3',
+                'message' => '本文3',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'user_id' => 4,
+                'title' => 'タイトル4',
+                'message' => '本文4',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]
         ]);
     }
 }
