da7ce059cafffe322b5072c39f37f56ca10a5923
hikarutsujinaka <hikaru.tsujinaka@gmail.com>
2021-08-18 06:08:18 +0000

    editアクション追記

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,12 +8,12 @@ use App\User;
 
 class UsersController extends Controller
 {
-     public function index()
+    
+    public function edit(Request $request)
     {
         
-        return view('welcome', [
-            'users' => $users,
-        ]);
+        $user = User::find($id);
+        return view('users.edit', compact('user'));
     }
     
     public function update(Request $request, $id)
