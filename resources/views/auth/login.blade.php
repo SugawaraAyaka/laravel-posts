d7a110d5375860755f84f1db0bf9ebd8c0a93a46
ayaka Sugawara <m.cans0kfi@gmail.com>
2021-08-19 22:37:18 +0900

    インデントの修正、不要な</form>の消去、{!! Form::token() !! }の記載があるため不要な{{ csrf_field() }}を消去

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,10 +5,9 @@
         <div class="form-wrap col-xs-6 col-lg-5 m-auto">
             <div class="form-group text-center">
                 <h2 class="logo-img mx-auto mb-5 mt-5">
-                ログイン
+                    ログイン
                 </h2>
             </div>
-            {{ csrf_field() }}
             {!! Form::open(['route' => 'login.post']) !!}
             {!! Form::token() !!}
             <div class="form-group">
@@ -21,9 +20,6 @@
             {!! Form::submit('ログインする', ['class' => 'btn btn btn-danger mt-2']) !!}
             {!! Form::close() !!}
             </div>
-
-            </form>
-
             <br>
             <p class="devise-link mt-3 mb-5">
                 アカウントをお持ちでないですか？ {!! link_to_route('signup', '新規登録する') !!}
