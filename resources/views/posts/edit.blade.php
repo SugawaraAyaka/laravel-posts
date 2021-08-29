924c3de77f25fa946633425293d5911e8b3c7a9a
hikarutsujinaka <hikaru.tsujinaka@gmail.com>
2021-08-29 10:49:57 +0000

    edit.blade.phpフォルダの編集

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -11,7 +11,7 @@
         　投稿の編集
         </div>
     　　<div class="card-body">
-        <form class="upload" id="new_post" enctype="multipart/form-data" action="{{route('posts.update',$post->id)}}"accept-charset="UTF-8" method="POST">
+        <form class="upload" id="new_post" enctype="multipart/form-data" action="{{route('post.update',$post->id)}}"accept-charset="UTF-8" method="POST">
            {{csrf_field()}}
             <div class="md-form">
                 <input class="form-control" placeholder="タイトル" type="text" name="title" value=" value="{{ old('title') ?? $post->title }}/>
